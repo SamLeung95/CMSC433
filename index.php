4db5f54cf5ca0eee8f8fd6e6a09ea1ab7d8976c8
Sam Leung <tsunle1@umbc.edu>
2016-04-07 01:40:35 -0400

    Added Fontawesome and made some styled collapsable menu

--- index.php
+++ index.php
@@ -2,7 +2,32 @@
 
 	<head>
 		<title></title>
-	</head>
+		<link rel="stylesheet" href="./font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="./styles/main.css">
+		<link rel="stylesheet" type="text/css" href="./styles/collapse.css">
 
+	</head>
+	<body>
+	
+		
+		<div class="collapse" id="Required">
+			<span class="collapse_label">Required<span>
+	
+				<div class="dropdown_button">
+					<i class="fa fa-caret-down"></i>
+				</div>
+				
+		</div>
+		
+		<div class="collapse" id="Math">
+			<span class="collapse_label">Math<span>
+
+				<div class="dropdown_button">
+					<i class="fa fa-caret-down"></i>
+				</div>
+
+		</div>
+	
+	</body>
 
 </html>
