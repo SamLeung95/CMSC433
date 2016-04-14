d6216b46d924c06fdf988d24f26dc0fa15425f50
Sam Leung <tsunle1@umbc.edu>
2016-04-14 14:22:28 -0400

    Added redirect and session

--- class_select.php
+++ class_select.php
@@ -1,9 +1,22 @@
 <?php
 
+	session_start();
 	include('./HelpfulFunctions.php');
 	$link = connectDB();
 	
-	$topic='';
+	$valid;
+	
+	if(!isset($_SESSION['firstname'],$_SESSION['lastname'],$_SESSION['phone'],$_SESSION['email'])){
+			
+			header('Location:./index.php');
+			exit;
+			
+	}
+	else{
+		
+		$valid=true;
+		
+	}
 ?>
 
 <script>
@@ -35,291 +48,300 @@
 	</head>
 	<body>
 	
-		<form action="">
-			<!--Required CMSC-->
-			<?php $topic="Required CMSC"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+		<?php
+		
+		if($valid==true){
+		?>
+			<form action="./summary.php">
+				<!--Required CMSC-->
+				<?php $topic="Required CMSC"; ?>
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--Required CMSC-->
-			
-			<!--Required Math-->
-			<?php $topic="Required Math"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Required CMSC-->
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
-					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--Required Math-->
-			
-			<!--Required Stat-->
-			<?php $topic="Required Stat"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+				<!--Required Math-->
+				<?php $topic="Required Math"; ?>
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--Required Stat-->
-			
-			<!--Required Science-->
-			<?php $topic="Required Science"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Required Math-->
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
-					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--Required Science-->
-			
-			<!--Additional Science-->
-			<?php $topic="Additional Science"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+				<!--Required Stat-->
+				<?php $topic="Required Stat"; ?>
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--Additional Science-->
-			
-			<!--Science With Lab-->
-			<?php $topic="Science With Lab"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Required Stat-->
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
-					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--Science With Lab-->
-			
-			<!--CMSC Elective-->
-			<?php $topic="CMSC Elective"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+				<!--Required Science-->
+				<?php $topic="Required Science"; ?>
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--CMSC Elective-->
-			
-			<!--CMSC Tech Elec-->
-			<?php $topic="CMSC Tech Elec"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Required Science-->
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
-					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--CMSC Tech Elec-->
-			
-			<!--Additional Math-->
-			<?php $topic="Additional Math"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+				<!--Additional Science-->
+				<?php $topic="Additional Science"; ?>
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-					<?php
-					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
-					
-				</li>
-			</ul>
-			<!--Additional Math-->
-			
-			<!--Tech Math Elective-->
-			<?php $topic="Tech Math Elective"; ?>
-			
-			<ul id="<?php $topic; ?>" class="menu_bar">
-				<li class="group" onclick="Expand(this)">
-					<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
-					<div class="dropdown_button">
-						<i class="fa fa-caret-down"></i>
-					</div>
-				</li>
-				<li class="expand_container">
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Additional Science-->
 				
-					<!--Gets content of field from DB and turns into checkboxes-->
+				<!--Science With Lab-->
+				<?php $topic="Science With Lab"; ?>
+				
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-					<?php
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Science With Lab-->
+				
+				<!--CMSC Elective-->
+				<?php $topic="CMSC Elective"; ?>
+				
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-						$arr=getAllClassesArray($link, $topic);
-						foreach($arr as $row){
-							echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
-								<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
-							<br>';
-						}
-					?>
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--CMSC Elective-->
+				
+				<!--CMSC Tech Elec-->
+				<?php $topic="CMSC Tech Elec"; ?>
+				
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
 					
-				</li>
-			</ul>
-			<!--Tech Math Elective-->
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--CMSC Tech Elec-->
+				
+				<!--Additional Math-->
+				<?php $topic="Additional Math"; ?>
+				
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
+					
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Additional Math-->
+				
+				<!--Tech Math Elective-->
+				<?php $topic="Tech Math Elective"; ?>
+				
+				<ul id="<?php $topic; ?>" class="menu_bar">
+					<li class="group" onclick="Expand(this)">
+						<label for="group" class="group_label"><?php print($topic); ?><span class="mask"></span></label>
+						<div class="dropdown_button">
+							<i class="fa fa-caret-down"></i>
+						</div>
+					</li>
+					<li class="expand_container">
+					
+						<!--Gets content of field from DB and turns into checkboxes-->
+						
+						<?php
+						
+							$arr=getAllClassesArray($link, $topic);
+							foreach($arr as $row){
+								echo '<input type="checkbox" name="' . $row['id'] . '" value="' . $row['id'] . '" id="' . $row['name'] . '_checkbox" class="class_checkbox">
+									<label for="' . $row['name'] . '_checkbox" class="checkbox_label">' . $row['name'] . '</label>
+								<br>';
+							}
+						?>
+						
+					</li>
+				</ul>
+				<!--Tech Math Elective-->
+				
+				
+				<input type="submit" value="Submit" class="submit_button">  
+				
+			</form>
 			
-			
-			<input type="submit" value="Submit" class="submit_button">  
-			
-		</form>
+<		<?php 
+		}
+		?>		
+		
 	</body>
 
 </html>
