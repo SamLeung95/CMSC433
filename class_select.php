612132d8ea40db3dd82d65d65917781d013b6cbe
Sam Leung <tsunle1@umbc.edu>
2016-04-14 14:38:57 -0400

    Changed variable names

--- class_select.php
+++ class_select.php
@@ -6,18 +6,18 @@
 	
 	$valid;
 	
-	/*
-	if(!isset($_SESSION['firstname'],$_SESSION['lastname'],$_SESSION['phone'],$_SESSION['email'])){
+	
+	if(!isset($_SESSION['fName'],$_SESSION['lName'],$_SESSION['phone'],$_SESSION['email'])){
 			
 			header('Location:./index.php');
 			exit;
 			
 	}
-	*/
 	
-		
+	
+	else{	
 		$valid=true;
-		
+	}	
 	
 ?>
 
